a9019491ea4cba1c8ff21e2cd39537ca18e01d0f
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-02-26 09:09:37 +0530

    update in career api

--- whiteboardtec/Career/views.py
+++ whiteboardtec/Career/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from Career.models import Job, Application
 from Career.serializer import JobSerializer, ApplicationSerializer
 
@@ -16,6 +19,15 @@ class JobViewSet(ModelViewSet):
             return Job.objects.all()
         else:
             return Job.objects.filter(is_active=True)
+
+    @action(detail=True, methods=['get'])
+    def applications(self, request, pk=None):
+        "Get applications for a job"
+        job = self.get_object()
+        applications = Application.objects.filter(job=job)
+        serializer = ApplicationSerializer(applications, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class ApplicationViewSet(ModelViewSet):
     "Viewset for Application"
